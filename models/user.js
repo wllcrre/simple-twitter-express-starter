87ddf2b8590b454da6643c563d6f6545b49b42bb
pcreem <creempepper@gmail.com>
2019-11-28 12:01:18 +0800

    setup

--- models/user.js
+++ models/user.js
@@ -1,8 +1,38 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
-  };
-  return User;
-};
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING
+  }, {})
+  User.associate = function (models) {
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.hasMany(models.Reply)
+
+    //被 User 追縱的人(明星) Followings
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+
+    //追縱 User 的人(粉絲) Followers
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
+
+  }
+  return User
+}
